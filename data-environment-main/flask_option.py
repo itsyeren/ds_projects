fb9cdbab5bd77520552ffb45343c661d7f11f668
itsyeren <itsyeren@gmail.com>
2025-12-29 00:09:35 +0300

    Handle FLASK_ENV based start modes

---
+++ data-environment-main/flask_option.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-docstring
+
+import os
+
+def start():
+    """returns the right message"""
+    env = os.getenv("FLASK_ENV")
+
+    if env == "development":
+        return "Starting in development mode..."
+    elif env == "production":
+        return "Starting in production mode..."
+    else:
+        return "Starting in empty mode..."
+
+if __name__ == "__main__":
+    print(start())
